df914d8581afaf4ac2c539784b19fd691685e2f3
Patrick Kubica <mail@patrickkubica.de>
2020-07-27 20:29:37 +0200

    move contact deletion to unique route

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -5,12 +5,12 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Contact controller.
  *
- * @Route("contact")
  */
 class ContactController extends Controller
 {
@@ -65,11 +65,8 @@ class ContactController extends Controller
      */
     public function showAction(Contact $contact)
     {
-        $deleteForm = $this->createDeleteForm($contact);
-
         return $this->render('contact/show.html.twig', array(
             'contact' => $contact,
-            'delete_form' => $deleteForm->createView(),
         ));
     }
 
@@ -81,7 +78,6 @@ class ContactController extends Controller
      */
     public function editAction(Request $request, Contact $contact)
     {
-        $deleteForm = $this->createDeleteForm($contact);
         $editForm = $this->createForm('AppBundle\Form\ContactType', $contact);
         $editForm->handleRequest($request);
 
@@ -93,44 +89,23 @@ class ContactController extends Controller
 
         return $this->render('contact/edit.html.twig', array(
             'contact' => $contact,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+            'edit_form' => $editForm->createView()
         ));
     }
 
     /**
      * Deletes a contact entity.
      *
-     * @Route("/{id}", name="contact_delete")
-     * @Method("DELETE")
+     * @Route("/delete/{id}", name="contact_delete")
      */
     public function deleteAction(Request $request, Contact $contact)
     {
-        $form = $this->createDeleteForm($contact);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($contact);
-            $em->flush();
-        }
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($contact);
+        $em->flush();
 
+        //$this->addFlash('notice', 'Todo removed');
         return $this->redirectToRoute('contact_index');
-    }
-
-    /**
-     * Creates a form to delete a contact entity.
-     *
-     * @param Contact $contact The contact entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Contact $contact)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('contact_delete', array('id' => $contact->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
     }
 }
